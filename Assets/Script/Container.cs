3dc9982b6da4b07befebc63018aa75316be9fa67
Bogay <pojay11523@gmail.com>
2019-03-23 22:18:30 +0800

    [Feature] generate blocks

---
+++ Assets/Script/Container.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Container : MonoBehaviour
+{
+	public GameObject block;
+	public GameObject blockGroup;
+	public int row;
+	public int col;
+	public float rowSize;
+	public float colSize;
+
+	private Transform self;
+	private GameObject[,] blockGrid;
+
+	// Use this for initialization
+	void Start()
+	{
+		// Transform cache
+		this.self = transform;
+
+		this.blockGrid = new GameObject[this.row, this.col];
+
+		Vector3 curr = transform.position;
+		GameObject temp;
+		for(int i=0 ; i<this.row ; i++)
+		{
+			for(int j=0 ; j<this.col ; j++)
+			{
+				Debug.Log("Spawn a block.");
+
+				// spawn a block
+				temp = Instantiate(block, curr, Quaternion.identity, blockGroup.transform);
+				temp.name = $"block-{i}-{j}";
+
+				// calculate the value
+				MathBlock mb =  temp.GetComponent<MathBlock>();
+				mb.calculate(Random.Range(1, 25));
+
+				this.blockGrid[i, j] = temp;
+
+				curr.x += this.colSize;
+			}
+			curr.x = self.position.x;
+			curr.y -= this.rowSize;
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
---
+++ Assets/Script/MathBlock.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MathBlock : MonoBehaviour
+{
+	public GameObject textObject;
+	public int value { get; private set; }
+	public bool isSelected;
+	private Text textFormula;
+	private Transform self;
+
+	void Awake()
+	{
+		this.value = 0;
+		this.isSelected = false;
+		GameObject go = Instantiate(textObject, transform.position, Quaternion.identity, GameObject.Find("Canvas").transform);
+		go.name = $"text-{gameObject.name}";
+		this.textFormula = go.GetComponent<Text>();
+		this.self = transform;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+
+	public void calculate(int v)
+	{
+		this.value = v;
+
+		int n; // temparary value for formula
+		int op = Random.Range(0, 4);
+
+		if(!this.textFormula) Debug.Log("Text not found!");
+
+		switch(op)
+		{
+			// plus
+			case 0:
+				n = Random.Range(1, v);
+				textFormula.text = $"{v - n} + {n}";
+				break;
+			// subtract
+			case 1:
+				n = Random.Range(1, v);
+				textFormula.text = $"{v + n} - {n}";
+				break;
+			// multiply
+			case 2:
+				n = Random.Range(1, v);
+				// insure n is a factor of v
+				while(v % n != 0) n++;
+				textFormula.text = $"{v / n} x {n}";
+				break;
+			// divide
+			case 3:
+				n = Random.Range(1, 5);
+				textFormula.text = $"{v * n} / {n}";
+				break;
+		}
+	}
+}
