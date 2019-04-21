c1a629471702c9e099eab78227dce7b8a95cdd80
Bogay <pojay11523@gmail.com>
2019-04-21 15:38:18 +0800

    finish bloom effect

---
+++ Assets/Script/Effect/BackgroundRainfall.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundRainfall : MonoBehaviour
+{
+    public GameObject rain;
+
+    [Header("Bounds")]
+    public float left;
+    public float right;
+    public float upper;
+    public float lower;
+
+    public float coolDown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(this.generate());
+    }
+
+    private IEnumerator generate()
+    {
+        while(true)
+        {
+            if(Random.Range(0, 1f) > 0.5f)
+            {
+                GameObject go = Instantiate(this.rain, new Vector3(Random.Range(this.left, this.right), this.upper), Quaternion.identity);
+                float sy = Random.Range(0.8f, 1.5f);
+                float sx = sy * Random.Range(0.5f, 0.8f);
+                go.transform.localScale = new Vector3(sx, sy, 1);
+
+                go.GetComponent<RainBlock>().speed = Random.Range(4f, 12f);
+
+                yield return new WaitForSeconds(this.coolDown);
+            }
+        }
+    }
+}
---
+++ Assets/Script/Effect/RainBlock.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class RainBlock : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.down * this.speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Bound"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
