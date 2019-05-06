17ab73005874e32c31b56f411fd14dfbfb5a0492
Bogay <pojay11523@gmail.com>
2019-05-06 15:51:20 +0800

    add quit button

--- Assets/Script/Event/GameStart.cs
+++ Assets/Script/Event/GameStart.cs
@@ -9,6 +9,7 @@ public class GameStart : MonoBehaviour
 	private void Start()
 	{
 		GameObject.Find("ButtonStart").GetComponent<Button>().onClick.AddListener(() => SceneManager.LoadScene("Main"));
+		GameObject.Find("ButtonQuit").GetComponent<Button>().onClick.AddListener(() => Application.Quit());
 		GameObject.Find("ButtonRepoLink").GetComponent<Button>().onClick.AddListener(() => Application.OpenURL("https://github.com/Bogay/click-the-same"));
 	}
 }
