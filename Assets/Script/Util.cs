a507fe71b62c446bae37900c4ba3e91d7015e5ec
Bogay <pojay11523@gmail.com>
2019-05-06 14:34:44 +0800

    add util

---
+++ Assets/Script/Util.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Util
+{
+    public static Vector2 unitVec2()
+    {
+        float x = Random.Range(-1f, 1f);
+        float y = Random.Range(-1f, 1f);
+
+        return new Vector2(x, y).normalized;
+    }
+}
