5e10124d2d95875d5b466713d6e54fdcd0c25fd9
Bogay <pojay11523@gmail.com>
2019-05-06 14:40:58 +0800

    Game manager
    - record players' score
    - add scene loaded event

--- Assets/Script/Manager/GameManager.cs
+++ Assets/Script/Manager/GameManager.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
+using DG.Tweening;
 public class GameManager : MonoBehaviour
 {
 	private static GameManager _instance;
 	public static GameManager instance { get { return _instance; } }
 
 	public Dictionary<int, Container> teams;
+	public Dictionary<int, int> scores;
+	public AudioClip bgm;
+	public int gameDuration;
 
 	void Awake()
 	{
@@ -19,6 +23,8 @@ public class GameManager : MonoBehaviour
 
 		_instance = this;
 		this.teams = new Dictionary<int, Container>();
+		this.scores = new Dictionary<int, int>();
+		SceneManager.sceneLoaded += this.onSceneLoaded;
 		DontDestroyOnLoad(this);
 	}
 
@@ -33,6 +39,17 @@ public class GameManager : MonoBehaviour
 		return this.teams[number];
 	}
 
+	public int queryScore(int number)
+	{
+		if(!this.scores.ContainsKey(number))
+		{
+			Debug.Log($"Team [{number}] haven't been registered.");
+			return 0;
+		}
+
+		return this.scores[number];
+	}
+
 	public void registerTeam(int number, Container team)
 	{
 		if(this.teams.ContainsKey(number))
@@ -42,5 +59,30 @@ public class GameManager : MonoBehaviour
 		}
 
 		this.teams[number] = team;
+	}
+
+	private void onSceneLoaded(Scene scene, LoadSceneMode mode)
+	{
+		string sceneName = scene.name;
+		switch(sceneName)
+		{
+			case "Menu":
+				DOTween.Init();
+				break;
+			case "Main":
+				// set BGM
+				AudioManager.instance.setBGM(this.bgm);
+				AudioManager.instance.playBGM();
+				break;
+			case "End":
+				this.scores.Clear();
+				foreach (var item in this.teams)
+					this.scores.Add(item.Key, item.Value.score);
+				this.teams = new Dictionary<int, Container>();
+				break;
+			default:
+				Debug.Log($"No event for {sceneName}");
+				break;
+		}
 	}
 }
