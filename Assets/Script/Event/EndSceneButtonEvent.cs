9fef748a86f6cb0d664ff8de3c86ea4cba3d87ac
Bogay <pojay11523@gmail.com>
2019-05-06 14:38:53 +0800

    game end scene

---
+++ Assets/Script/Event/EndSceneButtonEvent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class EndSceneButtonEvent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.Find("Button Restart").GetComponent<Button>().onClick.AddListener(() => SceneManager.LoadScene("Main"));
+        GameObject.Find("Button Quit").GetComponent<Button>().onClick.AddListener(() => Application.Quit());
+    }
+}
---
+++ Assets/Script/Event/ShowGameResult.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowGameResult : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Text textScore = GameObject.Find("TextScore").GetComponent<Text>();
+        Text textResult = GameObject.Find("TextResult").GetComponent<Text>();
+        int[] scores = { GameManager.instance.queryScore(0), GameManager.instance.queryScore(1) };
+
+        textScore.text = $"{scores[0]} v.s. {scores[1]}";
+        if(scores[0] > scores[1])
+            textResult.text = "P1 Win!";
+        else if(scores[0] < scores[1])
+            textResult.text = "P2 Win!";
+        else
+            textResult.text = "Draw";
+    }
+}
