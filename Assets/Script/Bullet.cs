f9b25cbe7bd2b5e9d58f4a6030ecea753ad6c3f1
Bogay <pojay11523@gmail.com>
2019-05-06 14:34:24 +0800

    add bullet

---
+++ Assets/Script/Bullet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Bullet : MonoBehaviour
+{
+	public MathBlock target;
+	public Vector3 targetPos;
+	public float speed;
+
+	private Transform self;
+
+	// Use this for initialization
+	void Start()
+	{
+		this.self = transform;
+		this.self.DOMove(this.targetPos, Vector3.Distance(this.targetPos, this.self.position) / speed).OnComplete(() => Destroy(gameObject));
+	}
+}
